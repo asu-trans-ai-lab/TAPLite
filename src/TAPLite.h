697e923a9415a27d9cbb2e6c771e1b6790c3adf0
jdlph@ubuntu22.04 <jdlph@hotmail.com>
2025-08-09 21:15:18 -0400

    format C++ source code using clang-format
    - add .clang-format;
    - remove TAPLite_0501_2025.exe from data;
    - remove TAPLite.zip from python.

--- src/TAPLite.h
+++ src/TAPLite.h
@@ -10,14 +10,13 @@
 extern "C" PATH_ENGINE_API void DTA_AssignmentAPI();
 extern "C" PATH_ENGINE_API void DTA_SimulationAPI();
 
-
-#include <fstream>
-#include <map>
-#include <string>
-#include <sstream>
-#include <vector>
 #include <algorithm>
 #include <cctype>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
 
 static int MinLineSearchIterations = 5;
 static int ActualIterations = 0;
@@ -47,16 +46,10 @@ public:
     std::map<std::string, int> FieldsIndices;
 
     CDTACSVParser()
-        : Delimiter{ ',' },
-        IsFirstLineHeader{ true },
-        m_bSkipFirstLine{ false },
-        m_bDataHubSingleCSVFile{ false },
-        m_bLastSectionRead{ false }
-    {
-    }
+        : Delimiter{','}, IsFirstLineHeader{true}, m_bSkipFirstLine{false}, m_bDataHubSingleCSVFile{false},
+          m_bLastSectionRead{false} {}
 
-    ~CDTACSVParser()
-    {
+    ~CDTACSVParser() {
         if (inFile.is_open())
             inFile.close();
     }
@@ -71,27 +64,17 @@ public:
     bool ReadSectionHeader(std::string s);
     bool ReadRecord_Section();
     std::vector<std::string> ParseLine(std::string line);
-    bool GetValueByFieldName(std::string field_name,
-        std::string& value,
-        bool required_field = true);
+    bool GetValueByFieldName(std::string field_name, std::string& value, bool required_field = true);
     template <class T>
-    bool GetValueByFieldName(std::string field_name,
-        T& value,
-        bool required_field = true,
-        bool NonnegativeFlag = true);
+    bool GetValueByFieldName(std::string field_name, T& value, bool required_field = true, bool NonnegativeFlag = true);
     template <class T>
-    bool GetValueByKeyName(std::string field_name,
-        T& value,
-        bool required_field = true,
-        bool NonnegativeFlag = true);
+    bool GetValueByKeyName(std::string field_name, T& value, bool required_field = true, bool NonnegativeFlag = true);
 };
 
 // definitions of CDTACSVParser member functions
-void CDTACSVParser::ConvertLineStringValueToIntegers()
-{
+void CDTACSVParser::ConvertLineStringValueToIntegers() {
     LineIntegerVector.clear();
-    for (unsigned i = 0; i < LineFieldsValue.size(); ++i)
-    {
+    for (unsigned i = 0; i < LineFieldsValue.size(); ++i) {
         std::string si = LineFieldsValue[i];
         int value = atoi(si.c_str());
 
@@ -101,53 +84,38 @@ void CDTACSVParser::ConvertLineStringValueToIntegers()
 }
 
 // Trim in place
-static inline void TrimString(std::string& s)
-{
+static inline void TrimString(std::string& s) {
     // Left trim
-    s.erase(
-        s.begin(),
-        std::find_if(s.begin(), s.end(), [](unsigned char ch) { return !std::isspace(ch); })
-    );
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) { return !std::isspace(ch); }));
 
     // Right trim
-    s.erase(
-        std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !std::isspace(ch); }).base(),
-        s.end()
-    );
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !std::isspace(ch); }).base(), s.end());
 }
 
-bool CDTACSVParser::OpenCSVFile(std::string fileName, bool b_required)
-{
+bool CDTACSVParser::OpenCSVFile(std::string fileName, bool b_required) {
     mFileName = fileName;
     inFile.open(fileName.c_str());
 
-    if (!inFile.is_open())
-    {
+    if (!inFile.is_open()) {
         // If this CSV file is required and cannot be opened, handle error
-        if (b_required)
-        {
+        if (b_required) {
             // g_program_stop(); // or throw an exception, or however you prefer
         }
         return false;
     }
 
     // If the first line is a header row, read and parse it
-    if (IsFirstLineHeader)
-    {
+    if (IsFirstLineHeader) {
         std::string s;
-        if (!std::getline(inFile, s))
-        {
+        if (!std::getline(inFile, s)) {
             // File is open but no header line found
             return false;
         }
 
         // -- Optional: Remove UTF-8 BOM if present --
-        static const unsigned char UTF8_BOM[3] = { 0xEF, 0xBB, 0xBF };
-        if (s.size() >= 3 &&
-            (unsigned char)s[0] == UTF8_BOM[0] &&
-            (unsigned char)s[1] == UTF8_BOM[1] &&
-            (unsigned char)s[2] == UTF8_BOM[2])
-        {
+        static const unsigned char UTF8_BOM[3] = {0xEF, 0xBB, 0xBF};
+        if (s.size() >= 3 && (unsigned char)s[0] == UTF8_BOM[0] && (unsigned char)s[1] == UTF8_BOM[1] &&
+            (unsigned char)s[2] == UTF8_BOM[2]) {
             s.erase(0, 3);
         }
 
@@ -155,8 +123,7 @@ bool CDTACSVParser::OpenCSVFile(std::string fileName, bool b_required)
         std::vector<std::string> FieldNames = ParseLine(s);
 
         // Trim each header field and store in FieldsIndices map
-        for (size_t i = 0; i < FieldNames.size(); ++i)
-        {
+        for (size_t i = 0; i < FieldNames.size(); ++i) {
             std::string tmp_str = FieldNames[i];
             TrimString(tmp_str);
 
@@ -169,32 +136,26 @@ bool CDTACSVParser::OpenCSVFile(std::string fileName, bool b_required)
 
     return true;
 }
-bool CDTACSVParser::ReadRecord()
-{
+bool CDTACSVParser::ReadRecord() {
     LineFieldsValue.clear();
 
-    if (inFile.is_open())
-    {
+    if (inFile.is_open()) {
         std::string s;
         std::getline(inFile, s);
-        if (s.length() > 0)
-        {
+        if (s.length() > 0) {
             LineFieldsValue = ParseLine(s);
             return true;
         }
-        else
-        {
+        else {
             return false;
         }
     }
-    else
-    {
+    else {
         return false;
     }
 }
 
-std::vector<std::string> CDTACSVParser::ParseLine(std::string line)
-{
+std::vector<std::string> CDTACSVParser::ParseLine(std::string line) {
     std::vector<std::string> SeperatedStrings;
     std::string subStr;
 
@@ -203,37 +164,31 @@ std::vector<std::string> CDTACSVParser::ParseLine(std::string line)
 
     std::istringstream ss(line);
 
-    if (line.find_first_of('"') == std::string::npos)
-    {
-        while (std::getline(ss, subStr, Delimiter))
-        {
+    if (line.find_first_of('"') == std::string::npos) {
+        while (std::getline(ss, subStr, Delimiter)) {
             SeperatedStrings.push_back(subStr);
         }
 
-        if (line.at(line.length() - 1) == ',')
-        {
+        if (line.at(line.length() - 1) == ',') {
             SeperatedStrings.push_back("");
         }
     }
-    else
-    {
-        while (line.length() > 0)
-        {
+    else {
+        while (line.length() > 0) {
             size_t n1 = line.find_first_of(',');
             size_t n2 = line.find_first_of('"');
 
-            if (n1 == std::string::npos &&
-                n2 == std::string::npos)  // last field without double quotes
+            if (n1 == std::string::npos && n2 == std::string::npos) // last field without double quotes
             {
                 subStr = line;
                 SeperatedStrings.push_back(subStr);
                 break;
             }
 
-            if (n1 == std::string::npos && n2 != std::string::npos)  // last field with double
-                // quotes
+            if (n1 == std::string::npos && n2 != std::string::npos) // last field with double
+                                                                    // quotes
             {
-                size_t n3 = line.find_first_of('"', n2 + 1);  // second double quote
+                size_t n3 = line.find_first_of('"', n2 + 1); // second double quote
 
                 // extract content from double quotes
                 subStr = line.substr(n2 + 1, n3 - n2 - 1);
@@ -242,35 +197,30 @@ std::vector<std::string> CDTACSVParser::ParseLine(std::string line)
                 break;
             }
 
-            if (n1 != std::string::npos && (n1 < n2 || n2 == std::string::npos))
-            {
+            if (n1 != std::string::npos && (n1 < n2 || n2 == std::string::npos)) {
                 subStr = line.substr(0, n1);
                 SeperatedStrings.push_back(subStr);
-                if (n1 < line.length() - 1)
-                {
+                if (n1 < line.length() - 1) {
                     line = line.substr(n1 + 1);
                 }
-                else  // comma is the last char in the line string, push an empty string to the back
-                      // of vector
+                else // comma is the last char in the line string, push an empty string to the back
+                     // of vector
                 {
                     SeperatedStrings.push_back("");
                     break;
                 }
             }
 
-            if (n1 != std::string::npos && n2 != std::string::npos && n2 < n1)
-            {
-                size_t n3 = line.find_first_of('"', n2 + 1);  // second double quote
+            if (n1 != std::string::npos && n2 != std::string::npos && n2 < n1) {
+                size_t n3 = line.find_first_of('"', n2 + 1); // second double quote
                 subStr = line.substr(n2 + 1, n3 - n2 - 1);
                 SeperatedStrings.push_back(subStr);
                 size_t idx = line.find_first_of(',', n3 + 1);
 
-                if (idx != std::string::npos)
-                {
+                if (idx != std::string::npos) {
                     line = line.substr(idx + 1);
                 }
-                else
-                {
+                else {
                     break;
                 }
             }
@@ -279,14 +229,9 @@ std::vector<std::string> CDTACSVParser::ParseLine(std::string line)
     return SeperatedStrings;
 }
 
-bool CDTACSVParser::GetValueByFieldName(std::string field_name,
-    std::string& value,
-    bool required_field)
-{
-    if (FieldsIndices.find(field_name) == FieldsIndices.end())
-    {
-        if (required_field)
-        {
+bool CDTACSVParser::GetValueByFieldName(std::string field_name, std::string& value, bool required_field) {
+    if (FieldsIndices.find(field_name) == FieldsIndices.end()) {
+        if (required_field) {
             // dtalog.output() << "[ERROR] Field " << field_name << " in file " << mFileName << "
             // does not exist. Please check the file." << '\n'; g_DTA_log_file << "[ERROR] Field "
             // << field_name << " in file " << mFileName << " does not exist. Please check the
@@ -294,22 +239,18 @@ bool CDTACSVParser::GetValueByFieldName(std::string field_name,
         }
         return false;
     }
-    else
-    {
-        if (LineFieldsValue.size() == 0)
-        {
+    else {
+        if (LineFieldsValue.size() == 0) {
             return false;
         }
 
         unsigned int index = FieldsIndices[field_name];
-        if (index >= LineFieldsValue.size())
-        {
+        if (index >= LineFieldsValue.size()) {
             return false;
         }
         std::string str_value = LineFieldsValue[index];
 
-        if (str_value.length() <= 0)
-        {
+        if (str_value.length() <= 0) {
             return false;
         }
 
@@ -321,37 +262,27 @@ bool CDTACSVParser::GetValueByFieldName(std::string field_name,
 // Peiheng, 03/22/21, to avoid implicit instantiations in flash_dta.cpp and main_api.cpp for this
 // template function only all the other non-inline functions are implemented in utils.cpp
 template <class T>
-bool CDTACSVParser::GetValueByFieldName(std::string field_name,
-    T& value,
-    bool required_field,
-    bool NonnegativeFlag)
-{
-    if (FieldsIndices.find(field_name) == FieldsIndices.end())
-    {
-        if (required_field)
-        {
-            //cout << "[ERROR] Field " << field_name << " in file " << mFileName.c_str()
-            // << " does not exist. Please check the file." << '\n'; 
+bool CDTACSVParser::GetValueByFieldName(std::string field_name, T& value, bool required_field, bool NonnegativeFlag) {
+    if (FieldsIndices.find(field_name) == FieldsIndices.end()) {
+        if (required_field) {
+            // cout << "[ERROR] Field " << field_name << " in file " << mFileName.c_str()
+            //  << " does not exist. Please check the file." << '\n';
         }
         return false;
     }
-    else
-    {
-        if (LineFieldsValue.size() == 0)
-        {
+    else {
+        if (LineFieldsValue.size() == 0) {
             return false;
         }
 
         int size = (int)(LineFieldsValue.size());
-        if (FieldsIndices[field_name] >= size)
-        {
+        if (FieldsIndices[field_name] >= size) {
             return false;
         }
 
         std::string str_value = LineFieldsValue[FieldsIndices[field_name]];
 
-        if (str_value.length() <= 0)
-        {
+        if (str_value.length() <= 0) {
             return false;
         }
 
@@ -360,8 +291,7 @@ bool CDTACSVParser::GetValueByFieldName(std::string field_name,
         T converted_value;
         ss >> converted_value;
 
-        if (/*!ss.eof() || */ ss.fail())
-        {
+        if (/*!ss.eof() || */ ss.fail()) {
             return false;
         }
 
@@ -379,16 +309,15 @@ bool CDTACSVParser::GetValueByFieldName(std::string field_name,
     }
 }
 
-void ExitMessage(const char* format, ...)
-{
-	va_list ap;
+void ExitMessage(const char* format, ...) {
+    va_list ap;
 
-	// vprintf(format, ap);
-	printf("\n");
+    // vprintf(format, ap);
+    printf("\n");
 
-	getchar();
+    getchar();
 
-	exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
 }
 
 // Function to allocate a 1D array
@@ -403,7 +332,7 @@ void* Alloc_1D(int dim1, size_t size) {
 // Function to allocate a 2D array
 void** Alloc_2D(int dim1, int dim2, size_t size) {
     void** Array = (void**)calloc(dim1 + 1, sizeof(void*));
-    int i;  // Loop variable declared outside for efficiency
+    int i; // Loop variable declared outside for efficiency
 
     if (Array == NULL) {
         ExitMessage("Cannot allocate memory for two-dimensional array of size %d by %d.\n", dim1, dim2);
@@ -420,7 +349,7 @@ void** Alloc_2D(int dim1, int dim2, size_t size) {
 
 // Function to free a 2D array
 void Free_2D(void** Array, int dim1, int dim2) {
-    int i;  // Loop variable declared outside for efficiency
+    int i; // Loop variable declared outside for efficiency
 
     for (i = 0; i <= dim1; i++) {
         free(Array[i]);
@@ -431,7 +360,7 @@ void Free_2D(void** Array, int dim1, int dim2) {
 // Function to allocate a 3D array
 void*** Alloc_3D(int dim1, int dim2, int dim3, size_t size) {
     void*** Array = (void***)calloc(dim1 + 1, sizeof(void**));
-    int i, j;  // Loop variables declared outside for efficiency
+    int i, j; // Loop variables declared outside for efficiency
 
     if (Array == NULL) {
         ExitMessage("Cannot allocate memory for three-dimensional array of size %d by %d by %d.\n", dim1, dim2, dim3);
@@ -455,8 +384,8 @@ void*** Alloc_3D(int dim1, int dim2, int dim3, size_t size) {
 
 // Function to free a 3D array
 void Free_3D(void*** Array, int dim1, int dim2, int dim3) {
-    int i, j;   // Loop variables declared outside for efficiency
-    void* p;    // Pointer variable declared outside the loop for efficiency
+    int i, j; // Loop variables declared outside for efficiency
+    void* p;  // Pointer variable declared outside the loop for efficiency
 
     // Free the innermost arrays (1D arrays)
     for (i = 0; i <= dim1; i++) {
